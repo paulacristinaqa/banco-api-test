dacbe560294018f471609802fa0bd0d43ec89c17
paulacristinaqa <paulaschunckdev@gmail.com>
2026-04-03 15:29:38 +0100

    Implementação de fixtures nos testes de Login e Transferências

--- test/transfers.test.js
+++ test/transfers.test.js
@@ -2,6 +2,8 @@ const request = require('supertest');//condições para chamr o supertest
 const { expect } = require('chai');//condições para chamar o chai
 require('dotenv').config(); //puxa as variáveis de ambiente do .env
 const { obterToken }= require ('../helpers/autenticacao')
+const   postTransferencias = require ('../fixtures/postTransferencias.json')
+ 
 
 
 describe('Transferências', () => {
@@ -12,91 +14,72 @@ describe('Transferências', () => {
       token = await obterToken ('julio.lima','123456')
       tokenSemPermissao = await obterToken('junior.lima','123456')
     })
-    it('Teste 1-Deve retornar 201 quando for efetuada uma transferência maior ou igual a R$10,00', async () => {         
+    it('Teste 1-Deve retornar 201 quando for efetuada uma transferência maior ou igual a R$10,00', async () => {   
+      const bodyTransferencias = {...postTransferencias}  //cópia superficial somente com campos simples, pora subobjetos   tem outro tipo de clone
+      
       const resposta = await request(process.env.BASE_URL)
         .post('/transferencias')
         .set('Content-Type','application/json')    
         .set('Authorization',`Bearer ${token}`)                
-        .send({
-          'contaOrigem':1,
-          'contaDestino': 3,
-          'valor':10,
-          'token':''
-        })                  
+        .send(bodyTransferencias)                  
       expect(resposta.status).to.equal(201); 
                 
     });
-    it('Teste 2-Deve retornar 422 quando for efetuada uma transferência menor ou igual a R$10,00', async () => {              
+    it('Teste 2-Deve retornar 422 quando for efetuada uma transferência menor ou igual a R$10,00', async () => {    
+      const bodyTransferencias = {...postTransferencias}
+      bodyTransferencias.valor = 7
+      
       const resposta = await request(process.env.BASE_URL)
         .post('/transferencias')
         .set('Content-Type','application/json')   
         .set('Authorization',`Bearer ${token}`)                   
-        .send({
-          'contaOrigem':1,
-          'contaDestino': 3,
-          'valor':9.99,
-          'token':''
-        })                  
+        .send(bodyTransferencias)                 
       expect(resposta.status).to.equal(422); 
       console.log(resposta.status);
       console.log(resposta.body);  
     });
     it('Teste 3-Deve retornar 404 quando for efetuada uma transferência com conta origem inválida', async () => {
-     const resposta = await request(process.env.BASE_URL)
-        .post('/transferencias')
-        .set('Content-Type','application/json')    
-        .set('Authorization',`Bearer ${token}`)                  
-        .send({
-          'contaOrigem':488,
-          'contaDestino': 3,
-          'valor':10,
-          'token':''
-        })                  
+      const bodyTransferencias = {...postTransferencias}
+      bodyTransferencias.contaOrigem = 4889
+        const resposta = await request(process.env.BASE_URL)
+          .post('/transferencias')
+          .set('Content-Type','application/json')    
+          .set('Authorization',`Bearer ${token}`)                  
+          .send(bodyTransferencias)                 
       expect(resposta.status).to.equal(404); 
       console.log(resposta.status);
       console.log(resposta.body);  
     });
     it('Teste 3-Deve retornar 404 quando for efetuada uma transferência com conta destino inválida', async () => {
+      const bodyTransferencias = {...postTransferencias}
+      bodyTransferencias.contaDestino = 5000
       const resposta = await request(process.env.BASE_URL)
         .post('/transferencias')
         .set('Content-Type','application/json')    
         .set('Authorization',`Bearer ${token}`)                  
-        .send({
-          'contaOrigem':1,
-          'contaDestino': 500,
-          'valor':10,
-          'token':''
-        })                  
+        .send(bodyTransferencias)              
       expect(resposta.status).to.equal(404); 
       console.log(resposta.status);
       console.log(resposta.body);  
     });
     it('Teste 5-Deve retornar 403 quando for efetuada uma transferência com usuário sem permissão', async () => {
+      const bodyTransferencias = {...postTransferencias}
       const resposta = await request(process.env.BASE_URL)
         .post('/transferencias')
         .set('Content-Type','application/json') 
         .set('Authorization',`Bearer ${tokenSemPermissao}`)                     
-        .send({
-          'contaOrigem':1,
-          'contaDestino': 3,
-          'valor':10,
-          'token':''
-        })                  
+        .send(bodyTransferencias)         
       expect(resposta.status).to.equal(403); 
       console.log(resposta.status);
       console.log(resposta.body);  
     });
-    // it('Teste 6-Deve retornar 500 quando for efetuada uma transferênciacom quando o servidor estiver com algum erro interno ', async () => {    //  
+    // it('Teste 6-Deve retornar 500 quando for efetuada uma transferênciacom quando o servidor estiver com algum erro interno ', async () => {   
+    // const bodyTransferencias = {...postTransferencias}   
     //   const resposta = await request('http://localhost:3000')
       //   .post('/transferencias')
       //   .set('Content-Type','application/json')  
       //   .set('Authorization',`Bearer ${token}`)                    
-      //   .send({
-      //     'contaOrigem':1,
-      //     'contaDestino': 3,
-      //     'valor':10,
-      //     'token':''
-      //   })                  
+      //   .send(bodyTransferencias)           
       // expect(resposta.status).to.equal(500); 
       // console.log(resposta.status);
       // console.log(resposta.body);  
